2f1da877f4a35fe3f728f576c3227caa5a5bcfcb
Lucas Saaler <lucassaaler00@gmail.com>
2022-07-25 21:44:18 -0400

    Added htmlRoutes requirement to server.js and GET functionality to htmlRoutes.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express =require("express")
 const PORT= process.env.PORT || 3001
+const htmlRoute=require("./routes/htmlRoutes")
 const app= express()
 
 app.use(express.static("public"))
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+
+app.use(htmlRoute)
 
 app.listen(PORT,()=>{
     console.log("App is listening on Port http://localhost:"+ PORT)
