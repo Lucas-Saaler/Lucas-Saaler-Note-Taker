224eb86fc97fcc0c86e20071d509ae2f48421779
Lucas Saaler <lucassaaler00@gmail.com>
2022-07-25 21:49:48 -0400

    Added apiRoutes as a requirement to server.js and added GET functionality for the API. Also added skeleton for POST functionality. Will flesh that out next

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express =require("express")
-const PORT= process.env.PORT || 3001
 const htmlRoute=require("./routes/htmlRoutes")
+const apiRoute=require("./routes/apiRoutes")
+const PORT= process.env.PORT || 3001
 const app= express()
 
 app.use(express.static("public"))
@@ -8,6 +9,7 @@ app.use(express.static("public"))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
+app.use(apiRoute)
 app.use(htmlRoute)
 
 app.listen(PORT,()=>{
